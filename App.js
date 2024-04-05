ebccefaebf1a921d3662f72f35263651aef71c98
Caio Duarte <md.caio@yahoo.com.br>
2024-04-04 21:24:34 -0300

    Arquivo App.js

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
+  let nome = 'Prof° Mário';
+
+  
+  let img = 'https://sujeitoprogramador.com/steve.png';
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{ color: '#FF0000', fontSize: 25, margin: 15 }}>Programador</Text>
+      <Image
+        source={{ uri: img }}
+        style={{ width: 300, height: 300 }}
+      />
+      <Text style={{ fontSize: 30 }}> {nome} </Text>
       <StatusBar style="auto" />
+
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
